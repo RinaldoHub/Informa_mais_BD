1ea566c721bba7fd1fe3215908428c22c498dca2
RinaldoHub <87709068+RinaldoHub@users.noreply.github.com>
2021-09-06 16:57:21 -0300

    Função UPDATE funcionando

--- teste.py
+++ teste.py
@@ -3,96 +3,26 @@ import mysql.connector #Importamos o concector MySQL que implementa o Banco
 import datetime
 
 
-#Aqui vai as credenciais do nosso banco. Isso varia entre as máquinas, então por enquanto não foquemos aqui
-#e sim nos códigos SQL
+#Aqui vai as credenciais do nosso banco. Serve para realizar a conexão do plugin do python com o BD
 mydb = mysql.connector.connect(
    host = "localhost",
    user = "root",
    password = "",
    database = ""
 )
-
 #O cursor precisa ser criado para que possamos executar os códigos SQL no Python
 #Lembra bastante os Objetos em Java.
 cursor = mydb.cursor(prepared=True)
 
-#O código de DDL está COMENTADO para não haver duplicidade ou erro nos testes
-"""
-#Aqui é onde a mágica acontece. Através dessa linha executando esse método, o que
-#estiver entre as aspas duplas será executado no Banco propriamente dito.
-cursor.execute("CREATE DATABASE INFORMA_MAIS")
-
-cursor.execute("USE INFORMA_MAIS")
-
-cursor.execute("CREATE TABLE Relato (\
-              Descricao VARCHAR(100) NOT NULL,\
-              ID_Relato INTEGER NOT NULL,\
-              Data DATETIME NOT NULL,\
-              Status BIT NOT NULL,\
-              PRIMARY KEY(ID_Relato)) ENGINE=InnoDB")\
-
-cursor.execute("CREATE TABLE Midia (\
-              ID_Relato INTEGER,\
-              Foto BLOB,\
-              Video BLOB) ENGINE=InnoDB")\
-
-cursor.execute("CREATE TABLE Problema (\
-              ID_Relato INTEGER NOT NULL,\
-              Categoria VARCHAR(20) NOT NULL,\
-              Subcategoria VARCHAR(20) NOT NULL,\
-              PRIMARY KEY(Categoria)) ENGINE=InnoDB")\
-
-cursor.execute("CREATE TABLE Localizacao (\
-              ID_Relato INTEGER NOT NULL,\
-              Rua VARCHAR(15) NOT NULL,\
-              Bairro VARCHAR(20) NOT NULL,\
-              Cidade VARCHAR(15) NOT NULL,\
-              CEP VARCHAR(10) NOT NULL) ENGINE=InnoDB")\
-
-cursor.execute("CREATE TABLE Funcionario_Publico (\
-              nome VARCHAR(30) NOT NULL,\
-              Orgao_empresa VARCHAR(15) NOT NULL,\
-              ID_Funcionario INTEGER NOT NULL,\
-              PRIMARY KEY(ID_Funcionario)) ENGINE=InnoDB")\
-
-cursor.execute("CREATE TABLE Endereco (\
-              email_cidadao VARCHAR(35) NOT NULL,\
-              Rua VARCHAR(25) NOT NULL,\
-              Bairro VARCHAR(20) NOT NULL,\
-              Cidade VARCHAR(15) NOT NULL,\
-              CEP VARCHAR(10) NOT NULL,\
-              Numero VARCHAR(4) NOT NULL) ENGINE=InnoDB")\
-
-cursor.execute("CREATE TABLE Cidadao (\
-              id_cidadao INTEGER NOT NULL,\
-              nome VARCHAR(30) NOT NULL,\
-              email VARCHAR(35) NOT NULL,\
-              senha VARCHAR(16) NOT NULL,\
-              data_nascimento DATE NOT NULL,\
-              telefone VARCHAR(12) NOT NULL,\
-              ID_Relato INTEGER NOT NULL,\
-              PRIMARY KEY(id_cidadao)) ENGINE=InnoDB")
-
-#Aqui está a CIRAÇÃO DAS FOREIGN KEYS
-cursor.execute("ALTER TABLE Cidadao ADD CONSTRAINT FK_Cidadao_Relato FOREIGN KEY (ID_Relato) REFERENCES Relato (ID_Relato)")
-
-cursor.execute("ALTER TABLE Problema ADD CONSTRAINT FK_Problema_Relato FOREIGN KEY (ID_Relato) REFERENCES Relato (ID_Relato)")
-
-cursor.execute("ALTER TABLE Endereco ADD CONSTRAINT FK_Endereco_Cidadao FOREIGN KEY (email_cidadao) REFERENCES Cidadao (email)")
-
-cursor.execute("ALTER TABLE Midia ADD CONSTRAINT FK_Midia_Relato FOREIGN KEY (ID_Relato) REFERENCES Relato (ID_Relato)")
-
-cursor.execute("ALTER TABLE Localizacao ADD CONSTRAINT FK_Localizacao_Relato FOREIGN KEY (ID_Relato) REFERENCES Relato (ID_Relato)")
-
-cursor.execute("INSERT INTO Cidadao VALUES(0, 'Jose', 'jose@email.com', 'jose1234', '15/05/1985', '83986571374')")
-"""
-#Esse método fetchall() recupera as linhas de uma consulta
-#e retorna uma lista de tuplas onde cada tupla é uma linha
-#mas o código funciona sem ele
-#myresult = cursor.fetchall()
+#Aqui o Python irá abrir o arquivo .sql com os comandos DDL para criação do nosso banco
+if mydb.database == 'None':
+   f = open("informa_mais.sql", "r")
+   for x in f:
+      cursor.execute(f)
+   f.close()
 
 #TELA DE INTRODUÇÃO e decoração pro código
-print("########## CONSOLE MYSQL ##########\nPara sair, digite -1\n")
+print("########## BEM-VINDO(A) AO INFORMA+ ##########\nPara sair, digite -1\n")
 print("BANCO DE DADOS ATIVO: %s\nUSUÁRIO: %s\n" % (mydb.database, mydb.user))
 """
 #TELA PRINCIPAL
@@ -134,9 +64,6 @@ def Cadastrar():
 
 #FUNÇÃO DA TABELA
 def MenuPrincipal():
-   # INICIALIZAÇÃO DE VARIÁVEIS
-   tabela = "0"
-
    while True:
          print("------------------------------------\n")
          print("########## MENU PRINCIPAL ##########")
@@ -278,7 +205,144 @@ def MenuPrincipal():
 
          #ATUALIZAR DADOS
          elif (numero == '3'):
-            print("")
+
+            print("------------------------------------------\n")
+            print("########## ATUALIZAÇÃO DE DADOS ##########")
+            print("1 - RELATO\n2 - CIDADÃO\n3 - FUNCIONÁRIO PÚBLICO\n4 - ENDEREÇO\n5 - PROBLEMA\n6 - LOCALIZAÇÃO\n")
+            tabela = input('Qual TABELA deseja ATUALIZAR DADOS?\n>')
+            while tabela != '-1':
+               if tabela == '1':
+                  print("-------------------- COLUNAS NA TABELA RELATO --------------------")
+                  cursor.execute("describe relato")
+                  for x in cursor:
+                     print(x)
+                  print("-------------------- DADOS NA TABELA RELATO --------------------")
+                  cursor.execute("select * from relato")
+                  for x in cursor:
+                     print(x)
+                  print("") #Quebra de Linha
+                  c = input("Digite a ID do RELATO que sofrerá a atualização: ")
+                  a = input("Digite a coluna que deseja atualizar: ")
+                  b = input("Digite o dado que deseja inserir: ")
+                  sql = "UPDATE RELATO SET %s = '%s' WHERE ID_RELATO = '%s'"
+                  val = (a, b, c)
+                  cursor.execute(sql % val)
+                  mydb.commit()
+                  val = (a, c)
+                  cursor.execute("SELECT * FROM RELATO WHERE ID_RELATO = '%s'" % c)
+                  for x in cursor:
+                     print(x)
+               elif tabela == '2':
+                  print("-------------------- COLUNAS NA TABELA CIDADÃO --------------------")
+                  cursor.execute("describe cidadao")
+                  for x in cursor:
+                     print(x)
+                  print("-------------------- DADOS NA TABELA CIDADÃO --------------------")
+                  cursor.execute("select * from cidadao")
+                  for x in cursor:
+                     print(x)
+                  print("")  # Quebra de Linha
+                  c = input("Digite a ID do CIDADÃO que sofrerá a atualização: ")
+                  a = input("Digite a coluna que deseja atualizar: ")
+                  b = input("Digite o dado que deseja inserir: ")
+                  sql = "UPDATE CIDADAO SET %s = '%s' WHERE ID_CIDADAO = '%s'"
+                  val = (a, b, c)
+                  cursor.execute(sql % val)
+                  mydb.commit()
+                  val = (a, c)
+                  cursor.execute("SELECT * FROM CIDADAO WHERE ID_CIDADAO = '%s'" % c)
+                  for x in cursor:
+                     print(x)
+               elif tabela == '3':
+                  print("-------------------- COLUNAS NA TABELA FUNCIONÁRIO PÚBLICO --------------------")
+                  cursor.execute("describe FUNCIONARIO_PUBLICO")
+                  for x in cursor:
+                     print(x)
+                  print("-------------------- DADOS NA TABELA FUNCIONÁRIO PÚBLICO --------------------")
+                  cursor.execute("select * from FUNCIONARIO_PUBLICO")
+                  for x in cursor:
+                     print(x)
+                  print("")  # Quebra de Linha
+                  c = input("Digite a ID do FUNCIONÁRIO PÚBLICO que sofrerá a atualização: ")
+                  a = input("Digite a coluna que deseja atualizar: ")
+                  b = input("Digite o dado que deseja inserir: ")
+                  sql = "UPDATE FUNCIONARIO_PUBLICO SET %s = '%s' WHERE ID_FUNCIONARIO = '%s'"
+                  val = (a, b, c)
+                  cursor.execute(sql % val)
+                  mydb.commit()
+                  val = (a, c)
+                  cursor.execute("SELECT * FROM FUNCIONARIO_PUBLICO WHERE ID_FUNCIONARIO = '%s'" % c)
+                  for x in cursor:
+                     print(x)
+               elif tabela == '4':
+                  print("-------------------- COLUNAS NA TABELA ENDEREÇO --------------------")
+                  cursor.execute("describe endereco")
+                  for x in cursor:
+                     print(x)
+                  print("-------------------- DADOS NA TABELA ENDEREÇO --------------------")
+                  cursor.execute("select * from endereco")
+                  for x in cursor:
+                     print(x)
+                  print("")  # Quebra de Linha
+                  c = input("Digite a ID do ENDEREÇO que sofrerá a atualização: ")
+                  a = input("Digite a coluna que deseja atualizar: ")
+                  b = input("Digite o dado que deseja inserir: ")
+                  sql = "UPDATE endereco SET %s = '%s' WHERE ID_endereco = '%s'"
+                  val = (a, b, c)
+                  cursor.execute(sql % val)
+                  mydb.commit()
+                  val = (a, c)
+                  cursor.execute("SELECT * FROM endereco WHERE ID_endereco = '%s'" % c)
+                  for x in cursor:
+                     print(x)
+               elif tabela == '5':
+                  print("-------------------- COLUNAS NA TABELA PROBLEMA --------------------")
+                  cursor.execute("describe problema")
+                  for x in cursor:
+                     print(x)
+                  print("-------------------- DADOS NA TABELA PROBLEMA --------------------")
+                  cursor.execute("select * from problema")
+                  for x in cursor:
+                     print(x)
+                  print("")  # Quebra de Linha
+                  c = input("Digite a ID do PROBLEMA que sofrerá a atualização: ")
+                  a = input("Digite a coluna que deseja atualizar: ")
+                  b = input("Digite o dado que deseja inserir: ")
+                  sql = "UPDATE problema SET %s = '%s' WHERE ID_problema = '%s'"
+                  val = (a, b, c)
+                  cursor.execute(sql % val)
+                  mydb.commit()
+                  val = (a, c)
+                  cursor.execute("SELECT * FROM problema WHERE id_problema = '%s'" % c)
+                  for x in cursor:
+                     print(x)
+               elif tabela == '6':
+                  print("-------------------- COLUNAS NA TABELA LOCALIZAÇÃO --------------------")
+                  cursor.execute("describe localizacao")
+                  for x in cursor:
+                     print(x)
+                  print("-------------------- DADOS NA TABELA LOCALIZAÇÃO --------------------")
+                  cursor.execute("select * from localizacao")
+                  for x in cursor:
+                     print(x)
+                  print("")  # Quebra de Linha
+                  c = input("Digite a ID da LOCALIZAÇÃO que sofrerá a atualização: ")
+                  a = input("Digite a coluna que deseja atualizar: ")
+                  b = input("Digite o dado que deseja inserir: ")
+                  sql = "UPDATE localizacao SET %s = '%s' WHERE ID_localizacao = '%s'"
+                  val = (a, b, c)
+                  cursor.execute(sql % val)
+                  mydb.commit()
+                  val = (a, c)
+                  cursor.execute("SELECT * FROM localizacao WHERE ID_localizacao = '%s'" % c)
+                  for x in cursor:
+                     print(x)
+
+               print("")  # Quebra de linha
+               print(cursor.rowcount,"Dado(s) INSERIDO(s)!\n")
+               print(
+                  "1 - RELATO 2 - CIDADÃO 3 - FUNCIONÁRIO PÚBLICO 4 - ENDEREÇO 5 - PROBLEMA 6 - LOCALIZAÇÃO")
+               tabela = input("Deseja inserir mais dados?\nSe sim, digite o número da tabela\nSe não, digite '-1'\n>")
 
          #PESQUISAR DADOS
          elif (numero == '4'):
@@ -451,7 +515,7 @@ def MenuPrincipal():
                      for x in cursor:
                         print(x)
                   elif posicao == '2':
-                     sql = "SELECT * FROM LOCALIZACAOELATO WHERE %s LIKE '%s'"
+                     sql = "SELECT * FROM LOCALIZACAO WHERE %s LIKE '%s'"
                      val = (coluna, "%" + pesq + "%",)
                      cursor.execute(sql % val)
                      for x in cursor:
@@ -467,50 +531,13 @@ def MenuPrincipal():
                print("1 - RELATO 2 - CIDADÃO 3 - FUNCIONÁRIO PÚBLICO 4 - ENDEREÇO5 - PROBLEMA 6 - LOCALIZAÇÃO")
                tabela = input("Deseja realizar outra pesquisa?\nSe sim, digite o número da tabela\nSe não, digite '-1'\n>")
 
+         #Fechar CONSOLE
          elif (numero == '5'):
+            print("########## APLICATIVO ENCERRADO ##########")
             break
          print("------------------------------------")
          MenuPrincipal()
 
-
-#Como o DATETIME funciona
-"""a = datetime.datetime.now().isoformat()
-print(a)
-sql = "INSERT INTO RELATO(data) VALUES(%a)"
-
-val = (a)
-cursor.execute(sql % val)
-mydb.commit()
-cursor.execute("select * from relato")
-for x in cursor:
-   print(x)"""
-
-#Esta é a forma como se deve formatar a função de PESQUISA em Python
-"""
-a = input("")
-b = input("")
-sql = "SELECT * FROM RELATO WHERE %s LIKE '%s'"
-val = (a, "%" + b + "%",)
-print(sql % val)
-cursor.execute(sql % val)
-for x in cursor:
-   print(x)
-"""
 #ORDEM DE MENUS
 #MenuLogin()
 MenuPrincipal()
-
-"""
-#Aqui está a INTERAÇÃO COM O USUÁRIO
-while True:
-   try: #O try é utilizado para "tentar" executar o código
-      command = input("SQL>") #Aqui o input será atribuído à variável "command"
-      if(command == '-1'): #caso o usuário digite -1 o console será fechado
-         print("########## CONSOLE MYSQL FECHADO ##########")
-         break
-      cursor.execute(command) #O cursor mandará executar o input dado em "command"
-      for x in cursor: #O for faz uma varredura no comando do input e printa o que resgatou
-         print(x)
-   except: #E caso o código haja erros de sintaxe, ele avisa e volta para o começo do loop
-      print("***ERRO DE SINTAXE SQL***")
-      pass"""
